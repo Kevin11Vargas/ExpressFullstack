fe3b266bd4cb19f4f6e74caea5c3b664311b3bd1
Kevin Fared Velazquez Vargas <L19540460@delicias.tecnm.mx>
2023-05-08 09:43:12 -0600

    metodo borrar

--- routes/routes.js
+++ routes/routes.js
@@ -1,4 +1,5 @@
 //cargue la conexion del grupo MySQL
+const { request, response } = require('express');
 const pool= require('../data/config');
 
 //ruta de la app
@@ -19,3 +20,40 @@ const router=app => {
 }
 //exportar el router
 module.exports=router;
+
+//mostrar un solo usuario por ID
+app.get('/users/:id', (request,response) =>{
+    const id =request.params.id;
+    pool.query('SELECT * FROM users WHERE id= ?', id,(error, result) => {
+        if (error) throw error;
+        response.send(result);
+    });
+});
+
+//agregar un nuevo usuario 
+app.post('/users', (request,response) => {
+    pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+        if (error) throw error;
+
+        response.status(201).send(`User added with ID: ${result.insertId}`);
+    });
+});
+
+app.put('/users/id',(request, response) => {
+    const id= request.params.id;
+
+    pool.query('UPDATE users SET ? WHERE id = ? ',[request.body, id], (error, result)=>{
+        if (error) throw error;
+
+        response.send ('User updated successfully. ');
+    }); 
+});
+
+//eliminar un usuario 
+app.delete('/users/:id',(request, response) => {
+    const id = request.params.id;
+    pool.query('DELETE FROM users WHERE ID=?', id,(error, result)=>{
+        if (error) throw error;
+        response.send('Users delated.');
+    });
+});
